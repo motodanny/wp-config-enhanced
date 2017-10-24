ce01b9d8b5ac8d767a7391479cee2db757384db3
motodanny <danny@thehonorroll.com>
2017-10-24 09:55:05 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # wp-config-enhanced
+
+A few enhancements to the default WordPress config.
---
+++ wp-config-sample.php
@@ -0,0 +1,9 @@
+<?php
+// ...
+
+// Disable theme file editing in Dashboard
+// https://codex.wordpress.org/Hardening_WordPress
+define('DISALLOW_FILE_EDIT', true);
+
+/* That's all, stop editing! Happy blogging. */
+// ...
