13b24e01298eff1bf754b20e017f03c01b6ea088
motodanny <danny@thehonorroll.com>
2019-10-10 09:59:44 -0500

    Caps and spacing fix for consistency

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -3,7 +3,7 @@
 
 // Disable theme file editing in Dashboard
 // https://codex.wordpress.org/Hardening_WordPress
-define('DISALLOW_FILE_EDIT', true);
+define( 'DISALLOW_FILE_EDIT', true );
 
 /* That's all, stop editing! Happy blogging. */
 // ...
